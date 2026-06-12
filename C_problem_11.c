867a755ef5912e2cc004614c50e68c81acc1957d
Arputhan-13 <karputhan@gmail.com>
2026-06-12 22:32:49 +0530

    Create C_problem_11.c

---
+++ C_problem_11.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int disp_total_odd_digits()
+{
+     int a, b, c=0;
+     printf("Enter the Number : ");
+     scanf("%d",&a);
+     while(a!=0)
+     {
+     b=a%10;
+     if(b%2==1)
+     {
+     c=c+1;
+     }
+     a=a/10;
+     }
+     return c;
+}
+
+int main()
+{
+    int y;
+    y=disp_total_odd_digits();
+    printf("%d",y);
+}
